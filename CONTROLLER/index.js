be6820a0b590c55fb4b0008d5e75a9b89522e32b
Aryan <manglaaryan178@gmail.com>
2022-11-23 14:25:18 +0530

    added github action

--- CONTROLLER/index.js
+++ CONTROLLER/index.js
@@ -1,29 +1,29 @@
 let lifts = [];
 let q = [];
-let input_floors = document.getElementById("input-floors");
-let input_lifts = document.getElementById("input-lifts");
+let input_floors = document.getElementById('input-floors');
+let input_lifts = document.getElementById('input-lifts');
 let no_of_floors;
 let no_of_lifts;
 let intervalId;
 let tempInterval;
 
-let form = document.getElementById("form");
-form.addEventListener("click", function (e) {
+let form = document.getElementById('form');
+form.addEventListener('click', function (e) {
   e.preventDefault();
 });
 
 function createLifts(n) {
   lifts = [];
   for (let i = 1; i <= n; i++) {
-    let lift = document.createElement("div");
-    lift.className = "lift";
-    lift.id = "l" + i;
-    let left_door = document.createElement("div");
-    let right_door = document.createElement("div");
-    left_door.classList.add("lift__door", "lift__door-left");
-    right_door.classList.add("lift__door", "lift__door-right");
-    left_door.id = "ld" + i;
-    right_door.id = "rd" + i;
+    let lift = document.createElement('div');
+    lift.className = 'lift';
+    lift.id = 'l' + i;
+    let left_door = document.createElement('div');
+    let right_door = document.createElement('div');
+    left_door.classList.add('lift__door', 'lift__door-left');
+    right_door.classList.add('lift__door', 'lift__door-right');
+    left_door.id = 'ld' + i;
+    right_door.id = 'rd' + i;
     lift.style.left = `${10 + 10 * i}%`;
     lift.appendChild(left_door);
     lift.appendChild(right_door);
@@ -38,35 +38,35 @@ function createLifts(n) {
 }
 
 function createFloor(floor_number) {
-  let container = document.getElementById("container");
+  let container = document.getElementById('container');
 
-  let new_div = document.createElement("div");
-  new_div.className = "floor";
-  new_div.id = "floor" + floor_number;
+  let new_div = document.createElement('div');
+  new_div.className = 'floor';
+  new_div.id = 'floor' + floor_number;
 
-  let new_up_btn = document.createElement("button");
-  let up_text = document.createTextNode("U");
+  let new_up_btn = document.createElement('button');
+  let up_text = document.createTextNode('U');
   // up_text.src = "./img/up-arrow-icon.png";
-  let new_down_btn = document.createElement("button");
-  let down_text = document.createTextNode("D");
+  let new_down_btn = document.createElement('button');
+  let down_text = document.createTextNode('D');
   // down_text.src = "./img/down-arrow-icon.png";
-  let new_br = document.createElement("br");
-  let new_hr = document.createElement("hr");
-  let new_span = document.createElement("span");
+  let new_br = document.createElement('br');
+  let new_hr = document.createElement('hr');
+  let new_span = document.createElement('span');
   let new_floor_text = document.createTextNode(floor_number);
   new_span.appendChild(new_floor_text);
-  new_span.className = "floor__number";
-  new_hr.className = "hr";
-  new_hr.id = "hr" + floor_number;
-  new_up_btn.classList.add("control-btn", "control-btn--up");
-  new_down_btn.classList.add("control-btn", "control-btn--down");
+  new_span.className = 'floor__number';
+  new_hr.className = 'hr';
+  new_hr.id = 'hr' + floor_number;
+  new_up_btn.classList.add('control-btn', 'control-btn--up');
+  new_down_btn.classList.add('control-btn', 'control-btn--down');
   // up_text.classList.add("arrow-icon");
   // down_text.classList.add("arrow-icon");
 
   new_up_btn.appendChild(up_text);
-  new_up_btn.id = "up" + floor_number;
+  new_up_btn.id = 'up' + floor_number;
   new_down_btn.appendChild(down_text);
-  new_down_btn.id = "down" + floor_number;
+  new_down_btn.id = 'down' + floor_number;
 
   new_div.appendChild(new_up_btn);
   new_div.appendChild(new_br);
@@ -80,9 +80,9 @@ function createFloor(floor_number) {
 function closeDoor(e) {
   let target_id = e.target.id;
   let lift_no = target_id[target_id.length - 1];
-  let left_door = document.getElementById("ld" + lift_no);
-  let right_door = document.getElementById("rd" + lift_no);
-  right_door.removeEventListener("webkitTransitionEnd", closeDoor);
+  let left_door = document.getElementById('ld' + lift_no);
+  let right_door = document.getElementById('rd' + lift_no);
+  right_door.removeEventListener('webkitTransitionEnd', closeDoor);
   left_door.style.transform = `translateX(0)`;
   right_door.style.transform = `translateX(0)`;
   left_door.style.transition = `all 2.5s ease-out`;
@@ -104,13 +104,13 @@ function doorAnimation(e) {
   let target_id = e.target.id;
   // NOTE :- BAD ASSUMPTION , MAX LIFTS = 9
   let lift_no = target_id[target_id.length - 1];
-  let lift = document.getElementById("l" + lift_no);
-  lift.removeEventListener("webkitTransitionEnd", doorAnimation);
-  let left_door = document.getElementById("ld" + lift_no);
-  let right_door = document.getElementById("rd" + lift_no);
-  left_door.removeEventListener("webkitTransitionEnd", doorAnimation);
-  right_door.removeEventListener("webkitTransitionEnd", doorAnimation);
-  right_door.addEventListener("webkitTransitionEnd", closeDoor);
+  let lift = document.getElementById('l' + lift_no);
+  lift.removeEventListener('webkitTransitionEnd', doorAnimation);
+  let left_door = document.getElementById('ld' + lift_no);
+  let right_door = document.getElementById('rd' + lift_no);
+  left_door.removeEventListener('webkitTransitionEnd', doorAnimation);
+  right_door.removeEventListener('webkitTransitionEnd', doorAnimation);
+  right_door.addEventListener('webkitTransitionEnd', closeDoor);
   left_door.style.transform = `translateX(-100%)`;
   right_door.style.transform = `translateX(100%)`;
   left_door.style.transition = `all 2.5s ease-out`;
@@ -137,7 +137,7 @@ function moveLift(lift, to) {
   lift.currentFloor = to;
   lift.moving = true;
   let lft = lift.lift;
-  lft.addEventListener("webkitTransitionEnd", doorAnimation);
+  lft.addEventListener('webkitTransitionEnd', doorAnimation);
   lft.style.transform = `translateY(${distance}%)`;
   let time = 2 * Math.abs(from - to);
   if (time === 0) {
@@ -148,28 +148,28 @@ function moveLift(lift, to) {
   }
   lft.style.transitionDuration = `${time}s`;
   console.log(
-    `Lift Number: ${lift_no} \n Floor: \n From: ${from} To: ${to} \n Time: ${time} sec`
+    `Lift Number: ${lift_no} \n Floor: \n From: ${from} To: ${to} \n Time: ${time} sec`,
   );
 }
 
 function save_click(e) {
   let clicked_on = e.target.id;
   let n;
-  if (clicked_on.startsWith("up"))
+  if (clicked_on.startsWith('up'))
     n = Number(clicked_on.substring(2, clicked_on.length));
-  else if (clicked_on.startsWith("down"))
+  else if (clicked_on.startsWith('down'))
     n = Number(clicked_on.substring(4, clicked_on.length));
   q.push(n);
 }
 
 function getButtons() {
-  let up_btn_list = document.getElementsByClassName("control-btn--up");
-  let down_btn_list = document.getElementsByClassName("control-btn--down");
+  let up_btn_list = document.getElementsByClassName('control-btn--up');
+  let down_btn_list = document.getElementsByClassName('control-btn--down');
   for (up_btn of up_btn_list) {
-    up_btn.addEventListener("click", save_click);
+    up_btn.addEventListener('click', save_click);
   }
   for (down_btn of down_btn_list) {
-    down_btn.addEventListener("click", save_click);
+    down_btn.addEventListener('click', save_click);
   }
 }
 
@@ -186,7 +186,7 @@ function make_lifts() {
 }
 
 function make_floors() {
-  container.innerHTML = "";
+  container.innerHTML = '';
   no_of_floors = input_floors.value;
 
   for (let i = 1; i <= no_of_floors; i++) {
@@ -195,11 +195,11 @@ function make_floors() {
 }
 
 function place_lifts() {
-  let first_floor = document.getElementById("floor1");
+  let first_floor = document.getElementById('floor1');
   for (let i = lifts.length - 1; i >= 0; i--) {
     first_floor.insertBefore(
       lifts[i].lift,
-      first_floor.childNodes[first_floor.childNodes.length - 1]
+      first_floor.childNodes[first_floor.childNodes.length - 1],
     );
   }
 }
@@ -221,7 +221,7 @@ function start() {
   lifts = [];
 
   if (input_floors.value > 15) {
-    alert("FLOORS CAN BE NO MORE THAN 15");
+    alert('FLOORS CAN BE NO MORE THAN 15');
     return;
   } else if (input_lifts.value > 5) {
     alert("LIFTS CAN'T BE MORE THAN 5");
@@ -237,5 +237,5 @@ function start() {
   intervalId = setInterval(check_for_scheduling, 100);
 }
 
-let input_btn = document.getElementById("input-btn");
-input_btn.addEventListener("click", start);
+let input_btn = document.getElementById('input-btn');
+input_btn.addEventListener('click', start);
